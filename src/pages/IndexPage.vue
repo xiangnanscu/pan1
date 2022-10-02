e19478928a94beda4a600a2aacadf5944dc54420
xn <280145668@qq.com>
2022-10-03 00:32:48 +0800

    ${msg:-push}

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,32 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <div>
+    <q-btn color="primary" label="Get Picture" @click="captureImage" />
+    <q-btn color="primary" label="calculate" @click="cal" />
+    {{cnt}}
+    <img :src="imageSrc">
+  </div>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'IndexPage'
-})
+<script setup>
+import { ref } from 'vue'
+import { Camera, CameraResultType } from '@capacitor/camera'
+const imageSrc = ref('')
+const cnt = ref(0)
+function cal() {
+  console.log("cal ...")
+  cnt.value++
+}
+async function captureImage() {
+  const image = await Camera.getPhoto({
+    quality: 90,
+    allowEditing: true,
+    resultType: CameraResultType.Uri
+  })
+  console.log("take ...")
+  // The result will vary on the value of the resultType option.
+  // CameraResultType.Uri - Get the result from image.webPath
+  // CameraResultType.Base64 - Get the result from image.base64String
+  // CameraResultType.DataUrl - Get the result from image.dataUrl
+  imageSrc.value = image.webPath
+}
 </script>
